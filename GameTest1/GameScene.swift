567688e162ec474c44b3da83e2dbe5020879fd72
Dan Goyette <dan.goyette@gmail.com>
2015-05-31 19:01:05 -0400

    Add draggable circles

--- GameTest1/GameScene.swift
+++ GameTest1/GameScene.swift
@@ -11,30 +11,23 @@ import SpriteKit
 class GameScene: SKScene {
     
     var gridSquareName = "gridSquare"
+    var pieceName = "pieceSquare"
+    var gameCircles = [SKShapeNode]()
+    var mainSquare = SKSpriteNode()
+    var rowCount = 5
+    var columnCount = 6
+    var gridSquareEdgeLength = 100
+
     
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 65
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        //self.addChild(myLabel)
-        
-        
         createTileGrid()
-        //demo()
     }
     
     func createTileGrid() {
         
-        let rowCount = 5
-        let columnCount = 6
-        let gridSquareEdgeLength = 100
-        let mainSquareWidth = columnCount * gridSquareEdgeLength
-        let mainSquareHeight = rowCount * gridSquareEdgeLength
+        var mainSquareWidth : Int = columnCount * gridSquareEdgeLength
+        var mainSquareHeight : Int = rowCount * gridSquareEdgeLength
         
-        let mainSquare = SKSpriteNode()
         mainSquare.size = CGSize(width: mainSquareWidth , height: mainSquareHeight)
         mainSquare.anchorPoint = CGPointMake(0,0)
         mainSquare.position = CGPoint(x:CGRectGetMidX(self.frame) - CGFloat(mainSquareWidth / 2),
@@ -66,52 +59,62 @@ class GameScene: SKScene {
                 gridSquare.name = self.gridSquareName
                 
                 mainSquare.addChild(gridSquare)
+                
+                
             }
         }
         
         self.addChild(mainSquare)
 
+//        for rowIndex in 0 ..< rowCount {
+//            for columnIndex in 0 ..< columnCount {
+//
+//        addCircleAtCoordinates( rowIndex, columnIndex: columnIndex)
+//            }
+//        }
+
+        addCircleAtCoordinates( 3, columnIndex: 2)
+        
+        addCircleAtCoordinates( 1, columnIndex: 4)
+        
+        
+
+        
+      
     }
     
-    
-    
-    func makeRedBox(size: CGSize) -> SKSpriteNode {
-        var node = SKSpriteNode()
-        node.color = UIColor.redColor()
-        node.size = size;
-        return node;
-    }
-    func makeGreenBox(size: CGSize) -> SKSpriteNode {
-        var node = SKSpriteNode()
-        node.color = UIColor.greenColor()
-        node.size = size;
-        return node;
-    }
-    func makeBlueBox(size: CGSize) -> SKSpriteNode {
-        var node = SKSpriteNode()
-        node.color = UIColor.blueColor()
-        node.size = size;
-        return node;
-    }
+    func addCircleAtCoordinates(rowIndex: Int, columnIndex: Int) {
    
-    
-    func demo() {
-        var box1 = self.makeRedBox(CGSizeMake(200, 200));
-        box1.anchorPoint = CGPointMake(0,0);
-        box1.position = CGPoint(x:CGRectGetMidX(self.frame) - 100, y:CGRectGetMidY(self.frame) - 100)
+        var radius = CGFloat(gridSquareEdgeLength / 4 )
+        var circle = SKShapeNode(circleOfRadius: radius) // Size of Circle
         
-        var box2 =  makeBlueBox(CGSizeMake(100, 100));
-        box2.anchorPoint = CGPointMake(0,0);
-        box2.position = CGPointMake(0,0);
-        box1.addChild(box2)
-            
-        var box3 =  makeGreenBox(CGSizeMake(25, 25));
-        box3.anchorPoint = CGPointMake(0,0);
-        box3.position = CGPointMake(0,0);
-        box2.addChild(box3)
+        circle.strokeColor = SKColor.blackColor()
+        circle.glowWidth = 1.0
+        circle.fillColor = SKColor.orangeColor()
         
-        self.addChild(box1)
+        circle.physicsBody?.dynamic = false
+        circle.physicsBody?.collisionBitMask = 0x0;
+        circle.physicsBody?.contactTestBitMask = 0x0;
+
+        gameCircles.append(circle)
+        self.addChild(circle)
+        
+        putCircleAtCoordinates(circle, rowIndex: rowIndex,columnIndex:columnIndex)
+
     }
+    func putCircleAtCoordinates(circle: SKShapeNode, rowIndex: Int, columnIndex: Int) {
+        
+        
+        // position of the circle is the base position of the main square, plus the
+        // row/column offsets, plus 1/4 the row/column offset to out the circle in the middle
+        // of the square.
+        var xPosition = mainSquare.position.x + CGFloat(columnIndex * gridSquareEdgeLength) + CGFloat(gridSquareEdgeLength / 2)
+        var yPosition = mainSquare.position.y + CGFloat(rowIndex * gridSquareEdgeLength) + CGFloat(gridSquareEdgeLength / 2)
+
+        circle.position = CGPointMake(xPosition,yPosition)
+
+    }
+    
     
     var lastSelectedNode : SKSpriteNode? = nil;
     var lastSelectedColor : UIColor? = nil;
@@ -119,52 +122,81 @@ class GameScene: SKScene {
     override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
         for touch in (touches as! Set<UITouch>) {
             let location = touch.locationInNode(self)
-            self.doNodeColorChange(location)
+            //self.doNodeColorChange(location)
+            
+            if (lastSelectedCircle != nil) {
+                lastSelectedCircle?.position = location
+            }
         }
     }
     
+    var lastSelectedCircle : SKShapeNode? = nil;
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         for touch in (touches as! Set<UITouch>) {
             let location = touch.locationInNode(self)
-            self.doNodeColorChange(location)
+            //self.doNodeColorChange(location)
+            
+            for node in self.nodesAtPoint(location) {
+                if let shapeNode = node as? SKShapeNode {
+                    if (contains (gameCircles, shapeNode)) {
+                        lastSelectedCircle = shapeNode
+                    }
+                }
+            }
+            //var currentNode = self.nodeAtPoint(location) as? SKSpriteNode
+            
         }
     }
     
     override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
-        self.revertLastSelectedNode();
-    }
-    
-    func doNodeColorChange(location: CGPoint) {
-        // Find the node at the touch postion, if one exists.
+        // TODO Move selected circle to proper position.
         
-        var currentNode = self.nodeAtPoint(location) as? SKSpriteNode
-        
-        if (currentNode != nil) {
-            
-            
-            if (currentNode!.name != gridSquareName) {
-                // If the current node isn't a gridSquareNode, then the only possible
-                // thing to do is reset the node's color, as long as lastSelectedNode
-                // isn't nil
-                
-                revertLastSelectedNode()
-            } else {
-                // Either the lastSelectedNode is nil or it's set to something.
-                // If it's nil, we don't worry about it,
-                // If it's something, and different from the current node, we revert it.
-                
-                if (currentNode != lastSelectedNode) {
-                    revertLastSelectedNode()
-                    
-                    self.lastSelectedNode = currentNode
-                    self.lastSelectedColor = currentNode!.color
-                    currentNode!.color = UIColor.purpleColor()
-                    
-                }
+        for touch in (touches as! Set<UITouch>) {
+            let location = touch.locationInNode(self)
+
+            if (lastSelectedCircle != nil) {
+                var xPosition = Int(floor((location.x - mainSquare.position.x) / CGFloat(gridSquareEdgeLength)))
+                var yPosition = Int(floor((location.y - mainSquare.position.y) / CGFloat(gridSquareEdgeLength)))
+                putCircleAtCoordinates(lastSelectedCircle!, rowIndex: yPosition, columnIndex: xPosition)
+                lastSelectedCircle = nil;
             }
         }
-
+        
+        
+        //self.revertLastSelectedNode();
     }
+    
+//    func doNodeColorChange(location: CGPoint) {
+//        // Find the node at the touch postion, if one exists.
+//        
+//        var currentNode = self.nodeAtPoint(location) as? SKSpriteNode
+//        
+//        if (currentNode != nil) {
+//            
+//            
+//            if (currentNode!.name != gridSquareName) {
+//                // If the current node isn't a gridSquareNode, then the only possible
+//                // thing to do is reset the node's color, as long as lastSelectedNode
+//                // isn't nil
+//                
+//                //revertLastSelectedNode()
+//            } else {
+//                // Either the lastSelectedNode is nil or it's set to something.
+//                // If it's nil, we don't worry about it,
+//                // If it's something, and different from the current node, we revert it.
+//                
+//                if (currentNode != lastSelectedNode) {
+//                    revertLastSelectedNode()
+//                    
+//                    self.lastSelectedNode = currentNode
+//                    self.lastSelectedColor = currentNode!.color
+//                    currentNode!.color = UIColor.purpleColor()
+//                    
+//                }
+//            }
+//        }
+//
+//    }
     
     
     func revertLastSelectedNode() {
